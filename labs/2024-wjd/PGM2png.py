edfc6a4673d3c305fbb82dba161e9cb9a45236e7
OliverWu515 <oliverwu2003@foxmail.com>
2025-01-19 08:32:22 +0800

    新增实验原本任务文件以资对照

--- labs/2024-wjd/PGM2png.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-
-def read_img():
-    im = Image.open("eai_map_imu.pgm")    # 读取文件
-    im.save("output_image.png")
-    print(im.size)   # 输出图片大小
-
-if __name__ == "__main__":
-    read_img()     # 调用read_img()
